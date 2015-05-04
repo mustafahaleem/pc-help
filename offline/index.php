658e368946e55b64a2d18e47a8a061bbb50941ae
root <root@localhost.localdomain>
2015-05-03 23:27:07 -0400

    Created page folders

---
+++ offline/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+		<meta charset="utf-8">
+		<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,300' rel='stylesheet' type='text/css'>
+		<link href='/alexparkinson/assets/style/css/homepage.css' rel='stylesheet' type='text/css'>
+	</head>
+	<body>
+		<div class="page">
+			<?php echo file_get_contents("navigation.php"); ?>
+			<div class="content">
+			
+			</div>
+		
+			<?php echo file_get_contents("footer.php"); ?>
+		</div>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+		<script src="homepage.js"></script>
+	</body>
+</html>
