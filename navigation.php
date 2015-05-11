580c32442da5a6db138e99b098d0cc22609a8237
Kobrastachka <pnggameing@gmail.com>
2015-05-11 04:37:47 +0000

    redesigned navigation

--- navigation.php
+++ navigation.php
@@ -1,15 +1,69 @@
 <div class="navigation">
-   <ul class="nav">
-   	<div class="beforelogo">
-	   <li><a href="/alexparkinson/">Home</a></li>
-	   <li><a href="/alexparkinson/online/">Online Support</a></li>
-	   <li><a href="/alexparkinson/offline/">Local Support</a></li>
+	<div class="inner">
+		<a href="alexparkinson/" class="logo"><img src="assets/images/logo2.png" style="height: 150px;">Springfield<br>Tech Geeks</a>
+		<ul class="nav">
+		  <li><a href="/alexparkinson/">Home</a></li>
+		  <li>
+		   <a href="/alexparkinson/online/">Online Support</a>
+		   <ul>
+			   <li><a href="/alexparkinson/online/tutorials/">Tutorials</a></li>
+			   <li><a href="/alexparkinson/online/software/">Remote Software Support</a></li>
+			   <li><a href="/alexparkinson/online/tuneup/">Remote PC Tuneup</a></li>
+			   <li><a href="/alexparkinson/online/email/">Email Support</a></li>
+			   <li><a href="/alexparkinson/online/skype/">Skype Support</a></li>
+		   </ul>
+		  </li>
+		  <li>
+		   <a href="/alexparkinson/offline/">Local Support</a>
+		   <ul>
+			   <li><a href="/alexparkinson/offline/repair/">PC Repair</a>
+				   <ul>
+					   <li><a href="/alexparkinson/offline/repair/diagnostics/">Diagnosing Problems</a></li>
+					   <li><a href="/alexparkinson/offline/repair/upgrades/">Hardware Upgrades</a></li>
+					   <li><a href="/alexparkinson/offline/repair/hardware/">Hardware Replacement/Repair</a></li>
+					   <li><a href="/alexparkinson/offline/repair/os-upgrade/">Operating System Upgrade</a></li>
+					   <li><a href="/alexparkinson/offline/repair/os-restore/">Operating System Restore</a></li>
+					   <li><a href="/alexparkinson/offline/repair/antivirus/">Antivirus Install</a></li>
+				   </ul>
+			   </li>
+			   <li><a href="/alexparkinson/offline/malware/">Malware Removal</a>
+				   <ul>
+					   <li><a href="/alexparkinson/offline/malware/virus/">Virus Removal</a></li>
+					   <li><a href="/alexparkinson/offline/malware/spyware/">Spyware Removal</a></li>
+					   <li><a href="/alexparkinson/offline/malware/adware/">Adware Removal</a></li>
+				   </ul>
+			   </li>
+			   <li><a href="/alexparkinson/offline/tuneup/">PC Tuneup</a></li>
+			   <li><a href="/alexparkinson/offline/setup/">Hardware Setup</a>
+				   <ul>
+					   <li><a href="/alexparkinson/offline/setup/wireless/">Wireless Setup</a></li>
+					   <li><a href="/alexparkinson/offline/setup/printer/">Printer Setup</a></li>
+					   <li><a href="/alexparkinson/offline/setup/computer/">Computer Setup</a></li>
+				   </ul>
+			   </li>
+			   <li><a href="/alexparkinson/offline/drive/">Hard Drive Maintenance</a>
+				   <ul>
+					   <li><a href="/alexparkinson/offline/drive/backups/">Backups</a></li>
+					   <li><a href="/alexparkinson/offline/drive/transfer/">Data Transfer</a></li>
+					   <li><a href="/alexparkinson/offline/drive/disposal/">Disposal</a></li>
+				   </ul>
+			   </li>
+			   <li><a href="/alexparkinson/offline/custom/">Custom PCs</a>
+				   <ul>
+					   <li><a href="/alexparkinson/offline/custom/building/">PC Building</a></li>
+					   <li><a href="/alexparkinson/offline/custom/parts/">Part Recommendations</a></li>
+					   <li><a href="/alexparkinson/offline/custom/keyboard/">Cherry MX O-ring Modifications</a></li>
+				   </ul>
+			   </li>
+		   </ul>
+		  </li>
+		</ul>
+		<div class="sidebar">
+			<ul>
+				<li><a href="/alexparkinson/contact/">Contact Us</a></li>
+				<li><a href="/alexparkinson/about/">About Us</a></li>
+				<li><a href="/alexparkinson/contributors/">Contributors</a></li>
+			</ul>
+		</div>
 	</div>
-	<li><a href="/alexparkinson"><img src="/alexparkinson/assets/images/logo2.png" class="logo"></a></li>
-	<div class="afterlogo">
-	   <li><a href="/alexparkinson/contact/">Contact Us</a></li>
-	   <li><a href="/alexparkinson/about/">About Us</a></li>
-	   <li><a href="/alexparkinson/contributors/">Contributors</a></li>
-	</div>
-   </ul>
 </div>
