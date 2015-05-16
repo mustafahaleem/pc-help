2c03b98bdaa68ec21b95c64def714beca0bd0a72
Kobrastachka <pnggameing@gmail.com>
2015-05-16 18:19:58 +0000

    Updated Navigation and theme

--- navigation.php
+++ navigation.php
@@ -1,10 +1,16 @@
 <div class="navigation">
+	<div class="logo"><a href="/alexparkinson/"><img src="/alexparkinson/assets/images/logo.png" style="height: 150px;"></a></div>
+	<input type="checkbox" id="menu" hidden>
+	<div class="label">
+		<label for="menu"><span>Menu</span><span class="icon-menu"></span></label>
+	</div>
+	<div class="overlay"></div>
 	<div class="inner">
-		<a href="alexparkinson/" class="logo"><img src="/alexparkinson/assets/images/logo-placeholder.png" style="height: 150px;">Springfield<br>Tech Geeks</a>
 		<ul class="nav">
 		  <li><a href="/alexparkinson/">Home</a></li>
 		  <li>
-		   <a href="/alexparkinson/online/">Online Support</a>
+		   <input type="checkbox" class="expander" id="expander1" hidden>
+		   <label for="expander1" class="expander"><span class="link">Online Support<span></span></span></label>
 		   <ul>
 			   <li><a href="/alexparkinson/online/tutorials/">Tutorials</a></li>
 			   <li><a href="/alexparkinson/online/software/">Remote Software Support</a></li>
@@ -14,10 +20,13 @@
 		   </ul>
 		  </li>
 		  <li>
-		   <a href="/alexparkinson/offline/">Local Support</a>
+		   <input type="checkbox" class="expander" id="expander2" hidden>
+		   <label for="expander2" class="expander"><span class="link">Local Support<span></span></span></label>
 		   <ul>
-			   <li><a href="/alexparkinson/offline/repair/">PC Repair</a>
-				   <ul>
+			   <li>
+		   		<input type="checkbox" class="expander" id="expander3" hidden>
+		   		<label for="expander3" class="expander"><span class="link">PC Repair<span></span></span></label>
+		   			<ul>
 					   <li><a href="/alexparkinson/offline/repair/diagnostics/">Diagnosing Problems</a></li>
 					   <li><a href="/alexparkinson/offline/repair/upgrades/">Hardware Upgrades</a></li>
 					   <li><a href="/alexparkinson/offline/repair/hardware/">Hardware Replacement/Repair</a></li>
@@ -26,30 +35,38 @@
 					   <li><a href="/alexparkinson/offline/repair/antivirus/">Antivirus Install</a></li>
 				   </ul>
 			   </li>
-			   <li><a href="/alexparkinson/offline/malware/">Malware Removal</a>
-				   <ul>
+			   <li>
+		 			<input type="checkbox" class="expander" id="expander4" hidden>
+		  			 <label for="expander4" class="expander"><span class="link">Malware Removal<span></span></span></label>		   
+		  			<ul>
 					   <li><a href="/alexparkinson/offline/malware/virus/">Virus Removal</a></li>
 					   <li><a href="/alexparkinson/offline/malware/spyware/">Spyware Removal</a></li>
 					   <li><a href="/alexparkinson/offline/malware/adware/">Adware Removal</a></li>
 				   </ul>
 			   </li>
 			   <li><a href="/alexparkinson/offline/tuneup/">PC Tuneup</a></li>
-			   <li><a href="/alexparkinson/offline/setup/">Hardware Setup</a>
-				   <ul>
+			   <li>
+				  <input type="checkbox" class="expander" id="expander5" hidden>
+		  		 <label for="expander5" class="expander"><span class="link">Hardware Setup<span></span></span></label>
+		   			<ul>
 					   <li><a href="/alexparkinson/offline/setup/wireless/">Wireless Setup</a></li>
 					   <li><a href="/alexparkinson/offline/setup/printer/">Printer Setup</a></li>
 					   <li><a href="/alexparkinson/offline/setup/computer/">Computer Setup</a></li>
 				   </ul>
 			   </li>
-			   <li><a href="/alexparkinson/offline/drive/">Hard Drive Maintenance</a>
-				   <ul>
+			   <li>		
+				   <input type="checkbox" class="expander" id="expander6" hidden>
+		   			<label for="expander6" class="expander"><span class="link">Hard Drive Maintenance<span></span></span></label> 
+		   			<ul>
 					   <li><a href="/alexparkinson/offline/drive/backups/">Backups</a></li>
 					   <li><a href="/alexparkinson/offline/drive/transfer/">Data Transfer</a></li>
 					   <li><a href="/alexparkinson/offline/drive/disposal/">Disposal</a></li>
 				   </ul>
 			   </li>
-			   <li><a href="/alexparkinson/offline/custom/">Custom PCs</a>
-				   <ul>
+			   <li>
+				   <input type="checkbox" class="expander" id="expander7" hidden>
+		  		 <label for="expander7" class="expander"><span class="link">Custom PCs<span></span></span></label>	
+		 		  <ul>
 					   <li><a href="/alexparkinson/offline/custom/building/">PC Building</a></li>
 					   <li><a href="/alexparkinson/offline/custom/parts/">Part Recommendations</a></li>
 					   <li><a href="/alexparkinson/offline/custom/keyboard/">Cherry MX O-ring Modifications</a></li>
@@ -57,13 +74,9 @@
 			   </li>
 		   </ul>
 		  </li>
+			<li><a href="/alexparkinson/contact/">Contact Us</a></li>
+			<li><a href="/alexparkinson/about/">About Us</a></li>
+			<li><a href="/alexparkinson/contributors/">Contributors</a></li>
 		</ul>
-		<div class="sidebar">
-			<ul>
-				<li><a href="/alexparkinson/contact/">Contact Us</a></li>
-				<li><a href="/alexparkinson/about/">About Us</a></li>
-				<li><a href="/alexparkinson/contributors/">Contributors</a></li>
-			</ul>
-		</div>
 	</div>
 </div>
